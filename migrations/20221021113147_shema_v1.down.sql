96a7ad5438ba38db9031b6ce60c71344bfdb0a8a
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2022-10-25 13:45:26 +0200

    :rocket: Checkpoint to make an Frontend (merged shema)

--- migrations/20221021113147_shema_v1.down.sql
+++ migrations/20221021113147_shema_v1.down.sql
@@ -1,4 +1,8 @@
 -- Add down migration script here
+DROP TABLE equal_balances;
+DROP TABLE costs;
+DROP TABLE cost_shares;
+
 DROP TABLE users;
 alter table uploads
   DROP FOREIGN KEY (access_only_by_wg);
--- migrations/20221023080322_shema_v2.down.sql
+++
@@ -1,4 +0,0 @@
--- Add down migration script here
-DROP TABLE equal_balances;
-DROP TABLE costs;
-DROP TABLE cost_shares;
